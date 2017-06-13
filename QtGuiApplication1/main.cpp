d5dbb14758c0bd43e4e6c6c7834db3b3aee5a88c
cx3386 <cx3386@163.com>
2017-06-14 00:16:01 +0800

    version 1

--- QtGuiApplication1/main.cpp
+++ QtGuiApplication1/main.cpp
@@ -1,10 +1,13 @@
 #include "QtGuiApplication1.h"
-#include <QtWidgets/QApplication>
+#include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	QtGuiApplication1 w;
 	w.show();
+	printf("i fuck the world");
+	std::cout << "Hello World!" << std::endl;
 	return a.exec();
 }
