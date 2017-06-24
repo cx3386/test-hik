67fa158ddaffd6ad64cfcb541d4a4f74e63121f4
cx3386 <cx3386@163.com>
2017-06-24 23:55:38 +0800

    add a thread for image process; add a log system; not complete the procoess, not yet the thread

---
+++ CheckWheels/cimageprocess.cpp
@@ -0,0 +1,6 @@
+#include "cimageprocess.h"
+
+CImageProcess::CImageProcess()
+{
+
+}
---
+++ CheckWheels/cimageprocess.h
@@ -0,0 +1,11 @@
+#ifndef CIMAGEPROCESS_H
+#define CIMAGEPROCESS_H
+
+
+class CImageProcess : public QThread
+{
+public:
+    CImageProcess();
+};
+
+#endif // CIMAGEPROCESS_H
